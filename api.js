bf6c0aab52059290cce64e85a4f2822901a59beb
RoVixen <griffertyuiop@gmail.com>
2022-02-05 04:04:12 -0400

    register creado

--- api.js
+++ api.js
@@ -26,8 +26,40 @@ const consultas={
             
             return res.send({
                 mensaje:"Bienvenido",
-                result:true
+                success:true
             });
+        });
+    },
+    register:(req,res)=>{
+        if(!req.body.name)
+            return res.send({
+                mensaje:"Ingrese un nombre valido"
+            });
+        
+        if(!(/\w*[@]\w*[.]\w*/.test(req.body.email)))
+            return res.send({
+                mensaje:"Ingrese un email valido"
+            });
+
+        if(!req.body.password)
+            return res.send({
+                mensaje:"Ingrese una contraseña valida"
+            });
+        
+        if(req.body.password != req.body.repeat_password)
+            return res.send({
+                mensaje:"las contraseñas no coinciden"
+            });
+        
+        db.q("INSERT INTO users(name,password,email) values ("+
+        db.connection.escape(req.body.name)+","+
+        db.connection.escape(req.body.password)+","+
+        db.connection.escape(req.body.email)+")",(result)=>{
+            if(result.insertedId>0)
+                res.send({
+                    mensaje:"Te haz registrado con exito",
+                    success:true
+                });
         });
     }
 }
