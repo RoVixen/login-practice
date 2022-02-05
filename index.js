9a4f126470908eb487dc3d66d6c6909e457c9c7d
PyrEnd <endertev@gmail.com>
2022-02-05 02:46:02 -0300

    Static

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = 3000;
 
-app.get("/",(req,res)=>{
-    res.sendFile("./index.html");
+
+
+app.use(express.static('view'));
+
+app.get('/',(req,res)=>{
+    res.render('index');
 });
 
 app.listen(PORT,()=>{
-    console.log("listening in http://localhost:"+PORT);
+    console.log('listening in http://localhost:'+PORT);
 });
