758e8dd08b78068185849602eb9232718015ed92
RoVixen <griffertyuiop@gmail.com>
2022-02-05 01:22:42 -0400

    get y send file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/",(req,res)=>{
+    res.sendFile("./index.html");
+});
+
 app.listen(PORT,()=>{
     console.log("listening in "+PORT);
 });
